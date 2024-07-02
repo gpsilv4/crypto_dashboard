354e5b2e317428decc1e871d34697082f4152e27
Gonçalo Silva <goncalo_silv@hotmail.com>
2024-07-03 00:54:26 +0100

    added search by symbol and favourite currency

--- app/routes.py
+++ app/routes.py
@@ -28,15 +28,29 @@ def get_cryptos():
     data = response.json()
     return jsonify(data)
 
-@bp.route('/crypto/<name>', methods=['GET'])
-def get_crypto(name):
+@bp.route('/crypto/name/<query>', methods=['GET'])
+def get_crypto_by_name(query):
     url = f"{app.config['BASE_URL']}cryptocurrency/quotes/latest"
     headers = {
         'Accepts': 'application/json',
         'X-CMC_PRO_API_KEY': app.config['API_KEY'],
     }
     params = {
-        'slug': name.lower()
+        'slug': query.lower()
+    }
+    response = requests.get(url, headers=headers, params=params)
+    data = response.json()
+    return jsonify(data)
+
+@bp.route('/crypto/symbol/<query>', methods=['GET'])
+def get_crypto_by_symbol(query):
+    url = f"{app.config['BASE_URL']}cryptocurrency/quotes/latest"
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': app.config['API_KEY'],
+    }
+    params = {
+        'symbol': query.upper()
     }
     response = requests.get(url, headers=headers, params=params)
     data = response.json()
