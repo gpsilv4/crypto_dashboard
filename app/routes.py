97e1940f849e8d566b88d459b7667a03cead89e9
Gonçalo Silva <goncalo_silv@hotmail.com>
2024-07-02 18:07:27 +0100

    added functionality for currency variation, market cap, volume and currency search.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 # app/routes.py
-from flask import Blueprint, render_template, jsonify
+from flask import Blueprint, jsonify, render_template
 import requests
 from flask import current_app as app
 
@@ -9,6 +9,14 @@ bp = Blueprint('main', __name__)
 def index():
     return render_template('index.html')
 
+@bp.route('/search')
+def search():
+    return render_template('search.html')
+
+@bp.route('/list')
+def list_cryptos():
+    return render_template('list.html')
+
 @bp.route('/cryptos', methods=['GET'])
 def get_cryptos():
     url = f"{app.config['BASE_URL']}cryptocurrency/listings/latest"
@@ -17,5 +25,19 @@ def get_cryptos():
         'X-CMC_PRO_API_KEY': app.config['API_KEY'],
     }
     response = requests.get(url, headers=headers)
+    data = response.json()
+    return jsonify(data)
+
+@bp.route('/crypto/<name>', methods=['GET'])
+def get_crypto(name):
+    url = f"{app.config['BASE_URL']}cryptocurrency/quotes/latest"
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': app.config['API_KEY'],
+    }
+    params = {
+        'slug': name.lower()
+    }
+    response = requests.get(url, headers=headers, params=params)
     data = response.json()
     return jsonify(data)
